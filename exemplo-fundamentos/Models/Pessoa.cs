a59876a5d712696c361f10b75a4ff0871d95ddda
jefersongjr <jeferson_gjr@hotmail.com>
2022-12-13 20:50:11 -0300

    ✨ feat: usando a classe Pessoa

--- exemplo-fundamentos/Models/Pessoa.cs
+++ exemplo-fundamentos/Models/Pessoa.cs
@@ -8,11 +8,11 @@ namespace ExemploFundamentos.Models
     public class Pessoa
     {
         public string Nome {get; set;}
-        public int idade {get; set;}
+        public int Idade {get; set;}
 
         public void Apresentar()
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.")
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.");
         }
     }
 }
--- exemplo-fundamentos/Program.cs
+++ exemplo-fundamentos/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploFundamentos.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Jeferson";
+pessoa1.Idade = 31;
+pessoa1.Apresentar();

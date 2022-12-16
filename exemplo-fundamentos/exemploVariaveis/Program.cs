eec058ffe6f34c3740383ff6b0c87b58e3572245
jefersongjr <jeferson_gjr@hotmail.com>
2022-12-16 20:06:56 -0300

    ✨ feat: estudando tipos de váriaveis

---
+++ exemplo-fundamentos/exemploVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿string apresentacao = "Olá, boa noite!";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M; // representa o float sem cortar o zero
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10; // alterando o valor da variável.
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+
+Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
